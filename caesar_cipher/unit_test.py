93fd2eef1495c34f7d7557c741ee252ad5ffae0a
Andrew Zhang <andrewhzhang@gmail.com>
2023-02-17 16:28:49 -0800

    Added decrypt unit tests

--- caesar_cipher/unit_test.py
+++ caesar_cipher/unit_test.py
@@ -14,3 +14,13 @@ try:
     assert caesar_cipher.encrypt("ATTACKATONCE", 4) == "EXXEGOEXSRGI"
 except AssertionError:
     print("Error in test case 2")
+
+try:
+    assert caesar_cipher.decrypt("XYZABCDEFGHIJKLMNOPQRSTUVW", 23) == "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+except AssertionError:
+    print("Error in test case 1")
+
+try:
+    assert caesar_cipher.decrypt("EXXEGOEXSRGI", 4) == "ATTACKATONCE"
+except AssertionError:
+    print("Error in test case 2")
